99b190d56ea6528fd5cdf82926bb2494679381d7
Nuno Leite <nunocsleite@gmail.com>
2018-11-23 19:26:26 +0000

    Semana 11

---
+++ Semana 11/Aula-33/1-Interfaces/Interfaces.cs
@@ -0,0 +1,49 @@
+using System;
+
+namespace MyInterfaces
+{
+    interface I
+    {
+        void M();
+    }
+
+    public class A : I
+    {
+        public void M() { Console.WriteLine("A:M"); }
+    }
+
+    //public class B : A // 1
+    public class B : A, I  // 2
+    { 
+        new public void M() { Console.WriteLine("B:M"); }
+    }
+
+    public sealed class Program
+    {
+        public static void Main()
+        {
+            I i = new B();
+            i.M(); // Usando // 1 -> A:M
+                   // Usando // 2 -> B:M
+
+            A a = (A)i;
+            a.M(); // A::M
+
+            B b = (B)i;
+            b.M(); // B::M
+
+        }
+    }
+}
+/*
+Cenario 1
+A:M
+A:M
+B:M
+
+Cenario 2
+B:M
+A:M
+B:M
+
+*/ 
---
+++ Semana 11/Aula-33/2-EIMI/EIMI.cs
@@ -0,0 +1,53 @@
+using System;
+
+namespace EIMI
+{
+    interface IStorable
+    {
+        void Write();
+        void Read();
+    }
+    interface ITalk
+    {
+        void Talk();
+        void Read();
+    }
+    public class Document : IStorable, ITalk
+    {
+        // Torna IStorable::Read virtual (deixa de ser final)
+        public virtual void Read() { Console.WriteLine("Document::Read"); }
+        // /*public*/ /*virtual*/ void IStorable.Read() { Console.WriteLine("IStorable::Read"); } // Metodos explicitos nao podem ser publicos nem virtuais
+        // IStorable::Write é virtual final
+        public void Write() { Console.WriteLine("Document::Write"); }
+
+        // ITalk::Read virtual final e private de Document
+        void ITalk.Read() { Console.WriteLine("ITalk::Read"); }
+        // ITalk::Talk é virtual final
+        public void Talk() { Console.WriteLine("Document::Talk"); }
+    }
+
+    public sealed class Program
+    {
+        public static void Main1()
+        {
+            Document doc = new Document();
+            doc.Read();
+            doc.Talk();
+
+            IStorable isdoc = doc;
+            isdoc.Read();
+
+            ITalk itdoc = doc;
+            itdoc.Read();
+        }
+    }
+}
+
+/*
+Document::Read
+Document::Talk
+Document::Read
+ITalk::Read
+*/ 
+	
+	
